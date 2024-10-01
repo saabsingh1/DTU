b6159ef2547b277a006d5b57e0089be6a3248557
saabsingh1 <saab6a@gmail.com>
2024-10-01 10:33:21 +0200

    assignment 2 finished

--- 02269_process_mining/dependency_graph.py
+++ 02269_process_mining/dependency_graph.py
@@ -42,14 +42,9 @@ def dependency_graph_inline(log):
 
 def read_from_file(log): 
     XES_log = {}
-    try:
-        tree = ET.parse("extension-log.xes")  # Replace with your actual file path
-        root = tree.getroot()
-        print("File loaded successfully.")
-    except ET.ParseError as e:
-        print(f"Error parsing XML: {e}")
-    except FileNotFoundError:
-        print("The file was not found.")
+    
+    tree = ET.parse(log)  # Replace with your actual file path
+    root = tree.getroot()
 
     # Namespace used in the XML
     namespace = {'xes': 'http://www.xes-standard.org/'}
@@ -113,56 +108,3 @@ def dependency_graph_file(log):
     return dependency_dict
 
 
-
-
-f = """
-Task_A;case_1;user_1;2019-09-09 17:36:47
-Task_B;case_1;user_3;2019-09-11 09:11:13
-Task_D;case_1;user_6;2019-09-12 10:00:12
-Task_E;case_1;user_7;2019-09-12 18:21:32
-Task_F;case_1;user_8;2019-09-13 13:27:41
-
-Task_A;case_2;user_2;2019-09-14 08:56:09
-Task_B;case_2;user_3;2019-09-14 09:36:02
-Task_D;case_2;user_5;2019-09-15 10:16:40
-
-Task_G;case_1;user_6;2019-09-18 19:14:14
-Task_G;case_2;user_6;2019-09-19 15:39:15
-Task_H;case_1;user_2;2019-09-19 16:48:16
-Task_E;case_2;user_7;2019-09-20 14:39:45
-Task_F;case_2;user_8;2019-09-22 09:16:16
-
-Task_A;case_3;user_2;2019-09-25 08:39:24
-Task_H;case_2;user_1;2019-09-26 12:19:46
-Task_B;case_3;user_4;2019-09-29 10:56:14
-Task_C;case_3;user_1;2019-09-30 15:41:22"""
-
-'''
-log = log_as_dictionary(f)
-dg = dependency_graph_inline(log)
-
-for ai in sorted(dg.keys()):
-   for aj in sorted(dg[ai].keys()):
-       print(ai, '->', aj, ':', dg[ai][aj])
-'''
-
-
-
-log = read_from_file("extension-log.xes")
-
-for case_id in sorted(log):
-    print((case_id, len(log[case_id])))
-
-for k, v in sorted(log.items()): 
-    print(k,v)
-
-case_id = "case_123"
-event_no = 0
-print((log[case_id][event_no]["concept:name"], log[case_id][event_no]["org:resource"], log[case_id][event_no]["time:timestamp"],  log[case_id][event_no]["cost"]))
-
-
-dg = dependency_graph_file(log)
-
-for ai in sorted(dg.keys()):
-   for aj in sorted(dg[ai].keys()):
-       print(ai, '->', aj, ':', dg[ai][aj])
